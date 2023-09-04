ddfe3898b455e9f236f4c30ef638397be4d41be8
saikiranFriday <sai@fridayy.ai>
2023-09-04 20:31:24 +0530

    solved Module not found: Can't resolve @/components/brands

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import HeroBanner from '@/components/HeroBanner'
-import Brands from '@/components/brands'
 import HowItWorks from '@/components/HowItWorks'
 import SecondaryCTA from '@/components/SecondaryCTA'
 import FAQ from '@/components/FAQ'
